c6e9ab6b0ba951f1491c1c28ced5d2c37a31cf32
n.carbo@human-immobilier.fr <n.carbo@human-immobilier.fr>
2023-10-25 14:06:44 +0200

    modif css sur la navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,26 @@
 <template>
-    <h1>C'est le test de nuxt</h1>
+    
+    <nav class="navbar">
+    <!-- <RouterLink to="/add-idea">Nouvelle Page</RouterLink> -->
+    <div class="logo">
+      <img alt="Logo Boite a Idees" src="@/assets/images/logo.png" class="logo__img"/>
+      <div class="logo__title">
+        <button>
+          <p>Boîte à Idées</p>
+        </button>
+        <h2 class="logo__title-h2">HUMAN</h2>
+        <!-- <img alt="Vague" src="@/assets/images/separation-v2.svg" class="logo_wave" /> -->
+      </div>
+    </div>
+    <div class="button">
+      <RouterLink to="/idea/add">
+        <button class="button__add">
+          <p class="button__p">Ajouter une idée</p>
+          <img alt="icone-ajouter" src="@/assets/images/icon-ajouter.png" />
+        </button>
+      </RouterLink>
+    </div>
+  </nav>
 
     <RouterLink to="/">
         <button>ICI</button>
@@ -11,5 +32,5 @@
 </script>
 
 <style scoped>
-
+@import '@/assets/scss/navbar.scss';
 </style>
