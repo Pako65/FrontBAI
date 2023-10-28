a1d4508cdd8faf9a445e6a52d26f8759a7c16161
Pako65 <nico.carbo@orange.fr>
2023-10-28 12:33:16 +0200

    error cors

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,30 +2,38 @@
   <nav class="navbar">
     <!-- <RouterLink to="/add-idea">Nouvelle Page</RouterLink> -->
     <div class="navigation">
-      <img alt="Logo Boite a Idees" src="@/assets/images/logo.png" class="navigation__img" />
-      <div class="navigation__title">
-        <button class="navigation__bai">
-          <p>Boîte à Idées</p>
-        </button>
-        <h2 class="navigation__title-h2">HUMAN</h2>
-        <!-- <img alt="Vague" src="@/assets/images/separation-v2.svg" class="navigation_wave" /> -->
+      <div class="navigation__logo">
+        <img alt="Logo Boite a Idees" src="@/assets/images/logo.png" class="navigation__img" />
+        <div class="navigation__title">
+          <button class="navigation__button">
+            <p class="navigation__bai">Boîte à Idées</p>
+          </button>
+          <h2 class="navigation__human">HUMAN</h2>
+          <!-- <img alt="Vague" src="@/assets/images/separation-v2.svg" class="navigation_wave" /> -->
+        </div>
       </div>
-      <RouterLink to="/idea/add" class="navigation__button">
+      <RouterLink to="/idea/add" class="navigation__idea">
         <button class="navigation__add">
           <p class="navigation__p">Ajouter une idée</p>
-          <img alt="icone-ajouter" src="@/assets/images/icon-ajouter.png" />
+          <img alt="navigation__logoAdd" src="@/assets/images/icon-ajouter.png" />
         </button>
       </RouterLink>
     </div>
   </nav>
 
-  <section>
-    <div class="titreBody">
-      <h1 class="titreBody__titre">Liste des idées</h1>
-      <button class="titreBody__button">
-        <p>Trier</p>
+  <section class="listIdea">
+    <div class="listIdea__head">
+      <h1 class="listIdea__title">Liste des idées</h1>
+      <button class="listIdea__button">
+        <p class="listIdea__p">Trier</p>
         <img src="@/assets/images/icon-trier.png" alt="bouton trier">
       </button>
+    </div>
+
+    <div class="list">
+      <ul>
+        <li>{{ idea.title }}</li>
+      </ul>
     </div>
   </section>
 
@@ -35,7 +43,27 @@
 </template>
 
 <script>
-
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      idea: {},
+    };
+  },
+  mounted() {
+    this.fetchIdea();
+  },
+  methods: {
+    async fetchIdea() {
+      try {
+        const response = await axios.get('https://localhost:7182/api/getAll');
+        this.data = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+}
 </script>
 
 <style scoped>
