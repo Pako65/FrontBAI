80deaa37407882d5be12ac18d148ec918f530212
Pako65 <nico.carbo@orange.fr>
2023-11-05 12:01:10 +0100

    modif css + bouton home

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,15 +1,17 @@
 <template>
   <nav class="navbar">
     <div class="navigation">
-      <div class="navigation__logo">
-        <img alt="Logo Boite a Idees" src="@/assets/images/logo.png" class="navigation__img" />
-        <div class="navigation__title">
-          <button class="navigation__button">
-            <p class="navigation__bai">Boîte à Idées</p>
-          </button>
-          <h2 class="navigation__human">HUMAN</h2>
+      <RouterLink to="/idea" class="navigation__home">
+        <div class="navigation__logo">
+          <img alt="Logo Boite a Idees" src="@/assets/images/logo.png" class="navigation__img" />
+          <div class="navigation__title">
+            <button class="navigation__button">
+              <p class="navigation__bai">Boîte à Idées</p>
+            </button>
+            <h2 class="navigation__human">HUMAN</h2>
+          </div>
         </div>
-      </div>
+      </RouterLink>
       <RouterLink to="/idea/add" class="navigation__idea">
         <button class="navigation__add" v-if="showButton">
           <p class="navigation__p">Ajouter une idée</p>
